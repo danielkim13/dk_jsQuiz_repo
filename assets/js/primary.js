49b34b943723935478cf94112c83414671db436a
Daniel Kim <daniel.d.kim13@gmail.com>
2021-12-13 21:22:04 -0500

    small change, moved buttonEl down to the bottom

--- assets/js/primary.js
+++ assets/js/primary.js
@@ -1,21 +1,21 @@
-// write a function where the btn is clicked and timer starts
-
 const buttonEl = document.querySelector("#startButton");
-buttonEl.addEventListener("click", timerStart);
 
+// write a function where the btn is clicked and timer starts
 function timerStart() {
-  const timerEl = document.getElementById("timerBox");
-  //   ! ensure the timeLeft value back 60 seconds!!
-  let timeLeft = 5;
-  timerEl.className = "timer-box";
-  const timeInterval = setInterval(function () {
-    if (timeLeft > 0) {
-      timerEl.textContent = "Time:  " + timeLeft;
-      timeLeft--;
-    } else {
-      timerEl.textContent = "Time is Up";
-      clearInterval(timeInterval);
-      //   TODO send the page to where user can enter name and leads to score
-    }
-  }, 1000);
+    const timerEl = document.getElementById("timerBox");
+    //   ! ensure the timeLeft value back 60 seconds!!
+    let timeLeft = 5;
+    timerEl.className = "timer-box";
+    const timeInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            timerEl.textContent = "Time:  " + timeLeft;
+            timeLeft--;
+        } else {
+            timerEl.textContent = "Time is Up";
+            clearInterval(timeInterval);
+            //   TODO send the page to where user can enter name and leads to score
+        }
+    }, 1000);
 }
+
+buttonEl.addEventListener("click", timerStart);
