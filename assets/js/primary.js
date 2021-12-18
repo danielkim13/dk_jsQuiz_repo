ed956f3f8c0d82346586dd5fbc5793804b35a71e
Daniel Kim <daniel.d.kim13@gmail.com>
2021-12-17 23:43:14 -0500

    by the btn event, remove title and body of the first page and 3x divs in html for answer options later-on

--- assets/js/primary.js
+++ assets/js/primary.js
@@ -2,20 +2,33 @@ const buttonEl = document.querySelector("#startButton");
 
 // write a function where the btn is clicked and timer starts
 function timerStart() {
-    const timerEl = document.getElementById("timerBox");
-    //   ! ensure the timeLeft value back 60 seconds!!
-    let timeLeft = 5;
-    timerEl.className = "timer-box";
-    const timeInterval = setInterval(function () {
-        if (timeLeft > 0) {
-            timerEl.textContent = "Time:  " + timeLeft;
-            timeLeft--;
-        } else {
-            timerEl.textContent = "Time is Up";
-            clearInterval(timeInterval);
-            //   TODO send the page to where user can enter name and leads to score
-        }
-    }, 1000);
+  const timerEl = document.getElementById("timerBox");
+  //   ! ensure the timeLeft value back 60 seconds!!
+  let timeLeft = 5;
+  timerEl.className = "timer-box";
+  const timeInterval = setInterval(function () {
+    if (timeLeft > 0) {
+      timerEl.textContent = "Time:  " + timeLeft;
+      timeLeft--;
+    } else {
+      timerEl.textContent = "Time is Up";
+      clearInterval(timeInterval);
+      //   TODO send the page to where user can enter name and leads to score
+    }
+  }, 1000);
 }
 
 buttonEl.addEventListener("click", timerStart);
+buttonEl.addEventListener("click", wipeIt);
+
+// function that removes the h2 and p and button from the page after the start btn is pressed
+
+function wipeIt() {
+  const titleEl = document.getElementById("quizTitle");
+  const bodyEl = document.getElementById("quizBody");
+  const btnEl = document.getElementById("startButton");
+
+  titleEl.remove();
+  bodyEl.remove();
+  btnEl.remove();
+}
